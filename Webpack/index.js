fdf47e16d7277a1c4fac944a24bdcff783ed807c
JWang <liujun1994>
2020-09-04 12:05:30 +0800

    updated

---
+++ Webpack/index.js
@@ -0,0 +1,4 @@
+// 通过 CommonJS 规范导入 show 函数
+const show = require('./src/show.js');
+// 执行 show 函数
+show('Webpack');
---
+++ Webpack/src/show.js
@@ -0,0 +1,35 @@
+function show(content) {
+    window.document.getElementById('app').innerText = 'Hello,' + content;
+
+    let validator = {
+        set: function(obj, prop, value) {
+            if (prop === 'age') {
+                if (!Number.isInteger(value)) {
+                    throw new TypeError('The age is not an integer');
+                }
+                if (value > 200) {
+                    throw new RangeError('The age seems invalid');
+                }
+            }
+            // The default behavior to store the value
+            obj[prop] = value;
+            // 表示成功
+            return true;
+        }
+    };
+
+    let person = new Proxy({}, validator);
+
+    person.age = 100;
+
+    console.log(person.age);
+// 100
+    person.age = 'young';
+// 抛出异常: Uncaught TypeError: The age is not an integer
+    person.age = 300;
+// 抛出异常: Uncaught RangeError: The age seems invalid
+
+}
+
+// 通过 CommonJS 规范导出 show 函数
+module.exports = show;
