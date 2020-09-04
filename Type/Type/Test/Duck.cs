b1a9d784913e64bdf6354d07822130b0eb2a6001
JWang <liujun1994>
2020-09-04 11:47:28 +0800

    updted

---
+++ Type/Type/Test/Duck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class Duck : IComparable<Duck>
+    {
+        public int Size;
+        public int CompareTo(Duck other)
+        {
+            if (this.Size > other.Size)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ Type/Type/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Duck> ducks =new List<Duck>
+           {
+               new Duck{Size = 3},
+               new Duck{Size = 1},
+               new Duck{Size = 2},
+           };
+           ducks.Sort();
+           Console.ReadKey();
+        }
+    }
+}
